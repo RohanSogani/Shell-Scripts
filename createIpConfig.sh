79c83494262a69bbb71be7288bcbdcb4110a7874
RohanSogani <ronsogani@gmail.com>
2019-11-25 18:06:58 -0800

    Improvements to handle multiple clients

--- createIpConfig.sh
+++ createIpConfig.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 #Check if docker command exists
-if ! hash docker 2> /dev/null; then
-  echo "Docker command not found. Please install Docker"
-  exit 1
+if ! hash docker 2>/dev/null; then
+    echo "Docker command not found. Please install Docker"
+    exit 1
 fi
 
 file="ifconfig.txt"
 
-if [ -f $file ] ; then
-    echo "File Exists... Deleting File"
+if [ -f $file ]; then
+    echo "ifconfig file exists... Deleting File"
     rm $file
-    echo "Deleted"
+    printf "${GREEN}Deleted${NC}\n"
 fi
 
 #Command to get just the names of all the servers setup in docker
-OUTPUT="$(docker ps --format '{{.Names}}' | sort)"
+OUTPUT="$(docker ps --format '{{.Names}}' | grep -P "c[\d]+|s[\d]+" | sort)"
 #echo "${OUTPUT}"
 IPC=""
 echo "Server sequence --> IP"
-for server in ${OUTPUT};
-do
-  #Get just the IP Address of all the servers
-  #echo "$server"
-  if [ $server = "c1" ]
-  then
-    echo "Client found skip for now"
-    IPC="$(docker inspect $server | grep -Po '"IPAddress": *\K"([0-9]{1,3}[\.]){3}[0-9]{1,3}"')"
-    IPC="${IPC%\"}"
-    IPC="${IPC#\"}"
-    echo "$server --> ${IPC}"
-  else
-    IP="$(docker inspect $server | grep -Po '"IPAddress": *\K"([0-9]{1,3}[\.]){3}[0-9]{1,3}"')"
-    #Remove quotes
-    IP="${IP%\"}"
-    IP="${IP#\"}"
-    echo "$server --> ${IP}"
-    #Append in ifconfig,txt
-    echo "${IP}" >> $file
-  fi
+CLIENTS=""
+for server in ${OUTPUT}; do
+    #Get just the IP Address of all the servers
+    #echo "$server"
+    if ! [ "$server" = "${server#c}" ]; then
+        IPC="$(docker inspect $server | grep -Po '"IPAddress": *\K"([0-9]{1,3}[\.]){3}[0-9]{1,3}"')"
+        IPC="${IPC%\"}"
+        IPC="${IPC#\"}"
+        if [ -z "$CLIENTS" ]; then
+            CLIENTS="${IPC}"
+        else
+            CLIENTS="${CLIENTS}\n${IPC}"
+        fi
+        echo "$server --> ${IPC}"
+    else
+        IP="$(docker inspect $server | grep -Po '"IPAddress": *\K"([0-9]{1,3}[\.]){3}[0-9]{1,3}"')"
+        #Remove quotes
+        IP="${IP%\"}"
+        IP="${IP#\"}"
+        echo "$server --> ${IP}"
+        #Append in ifconfig,txt
+        echo "${IP}" >>$file
+    fi
 done
 echo "Put Client IP at the bottom"
-echo $IPC >> $file
-echo "$file Created!"
-
+echo -e $CLIENTS >>$file
+printf "${GREEN}$file Created!${NC}\n"
