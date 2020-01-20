d8cb3bc9357461abaad929f76ade1547c4e1bf84
RohanSogani <ronsogani@gmail.com>
2020-01-19 19:36:35 -0800

    Directory check validation

--- organizeDownloads.sh
+++ organizeDownloads.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-echo "Logged in as $USER"
-pathToClean='/Users/${USER}'
-echo "$pathToClean"
+# Colors used for logs
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+#Check if argument specified
 if [ $# -eq 0 ] 
 then
-    echo "Please provide one directory"
+    printf "${RED}Error: Please provide one directory${NC}\n"
     exit
 fi
-
 echo "Arg 1: $1"
+echo "Logged in as $USER"
+pathToClean="/Users/${USER}/${1}"
+echo "Directory to organize is $pathToClean"
+
+#Check if valid directory
+if [ -d $pathToClean ] 
+then
+    printf "${GREEN}Directory $pathToClean exists.${NC}\n" 
+else
+    printf "${RED}Error: Directory $pathToClean does not exists.${NC}\n"
+fi
